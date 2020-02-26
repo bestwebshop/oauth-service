80271667b74bdf6b890aea47f5d49124a04a0e4e
Wekra <github@cwernet.de>
2020-02-26 15:14:13 +0100

    Try to force a context relative redirectStrategy

--- src/main/java/tech/bestwebshop/api/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/tech/bestwebshop/api/authorizationserver/AuthorizationServerApplication.java
@@ -3,6 +3,9 @@ package tech.bestwebshop.api.authorizationserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +14,12 @@ public class AuthorizationServerApplication {
         SpringApplication.run(AuthorizationServerApplication.class, args);
     }
 
+
+    @Bean
+    public RedirectStrategy createRedirectStrategy(){
+        DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        redirectStrategy.setContextRelative(true);
+
+        return redirectStrategy;
+    }
 }
