316a5e29206ff9817b9508005797f47dd4cd027b
Wekra <github@cwernet.de>
2020-03-21 22:00:36 +0100

    Try to add CORS functionality

--- src/main/java/tech/bestwebshop/api/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/tech/bestwebshop/api/authorizationserver/AuthorizationServerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -19,6 +21,17 @@ public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 
 }
--- src/main/java/tech/bestwebshop/api/authorizationserver/config/SecurityConfig.java
+++ src/main/java/tech/bestwebshop/api/authorizationserver/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package tech.bestwebshop.api.authorizationserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,8 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/login", "eureka/**")
                 .permitAll();*/
         httpSecurity
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/oauth2/jwks.json", "/oauth2/introspect").permitAll()
+                .antMatchers("/oauth/token").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -34,6 +43,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout().permitAll();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("append","delete","entries","foreach","get",
+                "has","keys","set","values","Authorization", "Content-Type", "Referer", "Sec-Fetch-Dest", "User-Agent"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
    /* @Bean
